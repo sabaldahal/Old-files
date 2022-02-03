7ffc447556ace6f47f288a70caa8f6c57f5c23b4
dahalsl <98709842+dahalsl@users.noreply.github.com>
2022-02-02 19:11:25 -0500

    Add files via upload

---
+++ array custom 4d.py
@@ -0,0 +1,49 @@
+import math
+fidref = open('Refraction.txt','r')     #open file
+list = fidref.readlines()               #copy data
+fidref.close()                          #close file
+
+n1 = []                                 #declaring lists
+theta1 = []
+n2 = []
+theta2 = []
+theta2a = 0
+
+for i in range(1,len(list)):            #extracting data into those lists
+    separate = list[i].split()
+    n1.append(float(separate[0]))
+    theta1.append(float(separate[1]))
+    n2.append(float(separate[2]))
+
+for j in range(len(n1)):                                            #calculating the value of theta2
+    k = (n1[j]/n2[j])*(math.sin(math.radians(theta1[j])))
+    theta2a = math.degrees(math.asin(k))
+    theta2.append(float(theta2a))
+
+
+count = len(n1)+1
+#try 1 allData = [[0,0,0,0]*count] did not work        #declaring list of len(n1)+1 rows and 4 columns
+allData = [[0]*4 for i in range(count)]
+
+
+allData[0][0] = 'n1'                                        #adding the first row to the list
+allData[0][1] = 'Theta1'
+allData[0][2] = 'n2'
+allData[0][3] = 'Theta2'
+
+for o in range(len(n1)):                                    #adding values to the remaing rows of the list
+    allData[o+1][0]= ('{0:.3f}' .format(n1[o]))
+    allData[o+1][1]= ('{0:.1f}' .format(theta1[o]))
+    allData[o+1][2]= ('{0:.3f}' .format(n2[o]))
+    allData[o+1][3]= ('{0:.1f}' .format(theta2[o]))
+
+refractedfile = open('output.txt', 'w')               #opeining new file to write data
+
+for m in range(len(n1)+1):
+    refractedfile.write('{0} \n'.format(allData[m]))                         #writing the list allData to the new file
+    
+
+refractedfile.close()
+
+
+
